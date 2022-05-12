eb5e9c67516ff6d504278b9dcefcbbc3ae5f647f
zzoo0123 <ssj000123@gmail.com>
2022-05-12 13:01:09 +0900

    HelloWorld!

--- bj_2257/main.cpp
+++ bj_2257/main.cpp
@@ -6,9 +6,8 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+    printf("Hello World!");
 }

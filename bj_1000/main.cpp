17a1399bdaa687cbdefd87650a86a237bf076e23
zzoo0123 <ssj000123@gmail.com>
2022-05-12 13:26:36 +0900

    Initial Commit

---
+++ bj_1000/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  bj_1000
+//
+//  Created by suhseungju on 2022/05/12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
